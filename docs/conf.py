cde461c4edf023eee9fab0cb48e7852612159bc7
Reuven V. Gonzales <reuven@tobetter.us>
2012-01-30 16:53:00 -0800

    This could be it?

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,6 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 sys.path.append(os.path.abspath('_themes'))
-sys.path.append(os.path.abspath('.'))
 html_theme_path = ['_themes']
 html_theme = 'flask'
 
