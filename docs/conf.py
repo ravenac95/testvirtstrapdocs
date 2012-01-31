8030d56fcbfdb31138f5a31e19afc2cf7bc15a0a
Reuven V. Gonzales <reuven@tobetter.us>
2012-01-30 16:37:13 -0800

    boomer

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 sys.path.append(os.path.abspath('_themes'))
-html_theme_path = ['_themes']
+html_theme_path = ['./_themes', 'docs/_themes']
 html_theme = 'vlinux-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
